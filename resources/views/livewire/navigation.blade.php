4ef86ba335915fe8982d13766cb38ca3f0157bd0
Juan david perez sotelo <57807587+juanda70@users.noreply.github.com>
2023-03-11 15:03:19 -0500

    nav

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,8 +1,10 @@
 <header class="bg-trueGray-700">
     <div class='container flex items-center h-16'>
-        <a class="flex flex-col items-center justify-center bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
+        <a
+            class="flex flex-col items-center justify-center bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex" stroke-linecap="round"
+                    stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
             <span>{{ __('Categories') }}</span>
         </a>
@@ -10,5 +12,46 @@
             <x-application-mark class="block h-9 w-auto" />
         </a>
         @livewire('search')
+
+        <div class="ml-3 relative">
+            @auth
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+
+                        <button
+                            class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                            <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
+                                alt="{{ Auth::user()->name }}" />
+                        </button>
+
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <!-- Account Management -->
+                        <div class="block px-4 py-2 text-xs text-gray-400">
+                            {{ __('Manage Account') }}
+                        </div>
+
+                        <x-dropdown-link href="{{ route('profile.show') }}">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+
+                        <div class="border-t border-gray-200"></div>
+
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}" x-data>
+                            @csrf
+
+                            <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+                    </x-slot>
+                </x-dropdown>
+            @else
+            <x-dropdown align="right" width="48">
+            </x-dropdown>
+            @endauth
+        </div>
     </div>
 </header>
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,3 +1,6 @@
-<div class="flex-1">
+<div class="flex-1 relative">
     <x-input type="text" class="w-full" placeholder="{{ __('Search products') }}" />
+    <button class="absolute top-0 right-0 w-12 h-full bg-Orange-500 flex items-center justify-center rounded-r-md">
+        <x-search size="35" color='white'/>
+    </button>
 </div>
